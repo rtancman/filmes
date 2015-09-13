0019b4e11cf72358a4c77df25f4fa8df63469ee2
rtancman <rtancman@gmail.com>
2015-09-13 15:20:36 -0300

    iniciando configuracoes de banco, ignorando arquivos do sqlite

--- movies/settings.py
+++ movies/settings.py
@@ -8,9 +8,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+# Build paths inside the project
+from unipath import Path
+PROJECT_DIR = Path(__file__).parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -59,7 +59,7 @@ WSGI_APPLICATION = 'movies.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': PROJECT_DIR.child('database.db'),
     }
 }
 
